def54342b23c409db85ff7c31e55f99def9204f5
Avishka <gargavishka@gmail.com>
2021-06-10 23:37:20 +0530

    tailwind

--- flask/main.py
+++ flask/main.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-	return render_template("index.html")
+	# return render_template("index.html")
+	return render_template("tailwind.html")
 
 @app.route("/result", methods = ["GET", "POST"])
 def result():
