78134764b24b6c44f7428d57d1a98b9990fcb629
Avishka <gargavishka@gmail.com>
2021-06-10 21:10:45 +0530

    email sender

--- flask/main.py
+++ flask/main.py
@@ -1,15 +1,11 @@
 from flask import Flask,render_template, request
-import smtplib, ssl
+import smtplib
 
-# initialise flask
-app = Flask(__name__) 
+app = Flask(__name__)
 
-# routes
 @app.route("/")
 def index():
 	return render_template("index.html")
-
-# to check errors
 
 @app.route("/result", methods = ["GET", "POST"])
 def result():
@@ -19,30 +15,10 @@ def result():
 		reciever = request.form.get("reciever")
 		message = request.form.get("message")
 		server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+		server.ehlo()
 		server.login(sender, password)
 		server.sendmail(sender, reciever, message)
-		""" with smtplib.SMTP("smtp.gmail.com", 465) as connection:
-			# securing connection
-			connection.starttls()
-			connection.login(user=sender, password=password)
-			connection.sendmail(
-				from_addr=sender,
-				to_addrs=reciever,
-				msg="Subject : Hello\n\n\n\nThis is the body of my email"
-			) """
+		server.close()
 	return render_template("result.html")
 
 app.run(debug = True)
-
-""" my_email = "john@gmail.com"
-password = "abc"
-with smtplib.SMTP("smtp.gmail.com") as connection:
-    # securing connection
-    connection.starttls()
-    connection.login(user=my_email, password=password)
-    connection.sendmail(
-        from_addr=my_email,
-        to_addrs="abc@gmail.com",
-        msg="Subject : Hello\n\n\n\nThis is the body of my email"
-    )
-"""
